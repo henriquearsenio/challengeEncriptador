7dd3edaf38a93a1db4ba6a85334d8c4607910c5a
Henrique Arsenio <henrique.severiano@live.com>
2022-06-02 09:42:16 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,76 @@
+let entradaTexto = document.querySelector("#texto-entrada");
+let saidaTexto = document.querySelector("#texto-saida");
+
+entradaTexto.addEventListener("input", function(){
+    if(this.value.length == 0){
+        document.querySelector(".banner").classList.remove('esconder');
+        document.querySelector(".informacao").classList.remove('esconder');
+        document.querySelector("#botao-copiar").classList.add('esconder')
+        saidaTexto.value = "";
+    }
+})
+
+saidaTexto.addEventListener("input", function(){
+    if(this.value.length > 0){
+        document.querySelector("#botao-copiar").classList.remove('esconder');
+    }
+})
+
+function btnCriptografar(){
+    if(entradaTexto.value.length == 0){
+        alert("Você deve digitar um texto antes de Criptografar!");
+    } else {
+        saidaTexto.value = criptografar(entradaTexto.value);
+        document.querySelector(".banner").classList.add('esconder');
+        document.querySelector(".informacao").classList.add('esconder');
+        document.querySelector("#botao-copiar").classList.remove('esconder')
+    }
+}
+
+function btnDescriptografar(){
+    if(entradaTexto.value.length == 0){
+        alert("Você deve digitar um texto antes de Descriptografar!");
+    } else {
+        saidaTexto.value = descriptografar(entradaTexto.value);
+        document.querySelector(".banner").classList.add('esconder');
+        document.querySelector(".informacao").classList.add('esconder');
+    }
+}
+
+function criptografar(stringEncriptada){
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }     
+    }
+
+    return stringEncriptada;
+}
+
+function descriptografar(stringEncriptada){
+    let matrizCodigo = [
+        ["enter", "e"],
+        ["imes", "i"],
+        ["ai", "a"],
+        ["ober", "o"],
+        ["ufat", "u"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }     
+    }
+
+    return stringEncriptada;
+}
